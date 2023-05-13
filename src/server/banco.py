4f2a3c5a008f24e72844cdb50fc6b0e1f0b00e87
Renan A. de M. Rocha <renanrochamorais@outlook.com>
2023-05-13 11:56:52 -0300

    Adição da classe banco e função de cadastro de Conta

---
+++ src/server/banco.py
@@ -0,0 +1,24 @@
+from conta import Conta;
+from typing import Dict;
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Banco(metaclass=SingletonMeta):
+
+  contas: Dict[int, Conta] = {}
+  
+  def criarConta(self, numero : int) -> bool:
+    if(numero in self.contas):
+      return False;
+    else:
+      self.contas[numero] = Conta(numero, 0.0);
+      return True;
