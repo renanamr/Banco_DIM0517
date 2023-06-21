21083b48bf41244ed2b06efd71eb3aa5844bb9b2
joao <joaovmfa@gmail.com>
2023-06-21 14:48:34 -0300

    basic testing

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ cd src/server
 docker build -t back_end . > trash.txt 2>&1
 docker run -d --name backEnd back_end > trash.txt
 
-docker exec backEnd pytest 
+docker exec backEnd pytest -s
 
 docker stop backEnd > trash.txt
 docker rm backEnd > trash.txt
--- src/server/test/test_methods.py
+++ src/server/test/test_methods.py
@@ -1,12 +1,50 @@
 import pytest
+import server.services.banco as banco_test
 
-def add(a, b):
-    return a + b
 
-def test_add():
-    result = add(2, 3)
-    assert result == 5
+class TestBanco:
 
-    result = add(-1, 1)
-    assert result == 0
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        #setUp
 
+        yield 
+
+        #tearDown 
+        banco_test._contas = {}
+
+    def test_debito_1(self):
+        banco_test.criarConta(1,20)
+        assert banco_test._debito(1,20)[1] == 200
+        
+    
+    def test_debito_2(self):
+        banco_test.criarConta(1,20)
+        assert banco_test._debito(1,40)[1] == 400
+    
+    def test_debito_3(self):
+        assert banco_test._debito(1,20)[1] == 400
+    
+    def test_debito_4(self):
+        banco_test.criarConta(1,20)
+        assert banco_test._debito(1,-20)[1] == 400
+    
+    def test_transferencia_1(self):
+        banco_test.criarConta(1,20)
+        banco_test.criarConta(2,30)
+        assert banco_test._transferir(20,1,2)[1] == 200
+        
+    
+    def test_transferencia_2(self):
+        banco_test.criarConta(1,20)
+        banco_test.criarConta(2,30)
+        assert banco_test._transferir(30,1,2)[1] == 400
+    
+    def test_transferencia_3(self):
+        banco_test.criarConta(1,20)
+        assert banco_test._transferir(20,1,2)[1] == 400
+    
+    def test_transferencia_4(self):
+        banco_test.criarConta(1,20)
+        banco_test.criarConta(2,30)
+        assert banco_test._transferir(-20,1,2)[1] == 400
